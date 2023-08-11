991d7a0b05751f674d186e7ad9950615e8839e42
Mushishi Dickson <redgekson@gmail.com>
2023-08-11 09:01:09 +0200

    v13

--- src/App.jsx
+++ src/App.jsx
@@ -30,179 +30,210 @@ export const appContext = createContext();
 export const url = "https://byzantium-scorpion-cap.cyclic.app";
 
 function App() {
-  const { pathname } = useLocation();
+	const { pathname } = useLocation();
 
-  const [isAdmin, setIsAdmin] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [ischeckout, setIsCheckout] = useState(false);
-  const [products, setProducts] = useState([]);
-  const [categories, setCategories] = useState([]);
-  const [settings, setSettings] = useState({});
-  const [orders, setOrders] = useState([]);
-  const [reflesh, setReflesh] = useState(0);
-  const [userId, setUseId] = useState("");
-  const [error, setError] = useState("");
-  const [isError, setIsError] = useState(false);
-  const [cartProducts, setCartProducts] = useState([]);
-  const [wishlist, setWishlist] = useState([]);
-  const [openCart, setOpenCart] = useState(false);
-  const [quickView, setQuickView] = useState(false);
-  const [dataLoading, setDataLoading] = useState(false);
-  const [quickViewProduct, setQuickViewProduct] = useState({});
+	const [isAdmin, setIsAdmin] = useState(false);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [ischeckout, setIsCheckout] = useState(false);
+	const [products, setProducts] = useState([]);
+	const [categories, setCategories] = useState([]);
+	const [settings, setSettings] = useState({});
+	const [orders, setOrders] = useState([]);
+	const [reflesh, setReflesh] = useState(0);
+	const [userId, setUseId] = useState("");
+	const [error, setError] = useState("");
+	const [isError, setIsError] = useState(false);
+	const [cartProducts, setCartProducts] = useState([]);
+	const [wishlist, setWishlist] = useState([]);
+	const [openCart, setOpenCart] = useState(false);
+	const [quickView, setQuickView] = useState(false);
+	const [dataLoading, setDataLoading] = useState(false);
+	const [quickViewProduct, setQuickViewProduct] = useState({});
 
-  const onHrefChange = () => {
-    if (pathname.includes("/admin")) {
-      setIsAdmin(true);
-    } else {
-      setIsAdmin(false);
-    }
+	const onHrefChange = () => {
+		if (pathname.includes("/admin")) {
+			setIsAdmin(true);
+		} else {
+			setIsAdmin(false);
+		}
 
-    if (pathname.includes("/checkout")) {
-      setIsCheckout(true);
-    } else {
-      setIsCheckout(false);
-    }
-  };
+		if (pathname.includes("/checkout")) {
+			setIsCheckout(true);
+		} else {
+			setIsCheckout(false);
+		}
+	};
 
-  const getData = async () => {
-    setDataLoading(true);
-    try {
-      await axios.get(url + "/products").then(res => {
-        setProducts(res.data.reverse());
-      });
+	const getData = async () => {
+		setDataLoading(true);
+		let _products = [];
+		try {
+			await axios.get(url + "/products").then((res) => {
+				setProducts(res.data.reverse());
+				_products = [...res.data];
+			});
 
-      await axios.get(url + "/categories").then(res => {
-        setCategories(res.data);
-      });
+			await axios.get(url + "/categories").then((res) => {
+				setCategories(res.data);
+			});
 
-      await axios.get(url + "/settings").then(res => {
-        setSettings(res.data[0]);
-      });
-    } catch (error) {
-      setIsError(true);
-      setError("Something went wrong!");
-    }
+			await axios.get(url + "/settings").then((res) => {
+				setSettings(res.data[0]);
+			});
+		} catch (error) {
+			setIsError(true);
+			setError("Something went wrong!");
+		}
 
-    setDataLoading(false);
+		setDataLoading(false);
 
-    setCartProducts(prev => {
-      const array = JSON.parse(localStorage.getItem("cart")) || [];
-      return array;
-    });
+		setCartProducts((prev) => {
+			const array = [];
+			const temp = JSON.parse(localStorage.getItem("cart")) || [];
 
-    setWishlist(prev => {
-      const array = JSON.parse(localStorage.getItem("wishlist")) || [];
-      return array;
-    });
-  };
+			temp.map((t) => {
+				const item = _products.find(({ _id }) => _id === t);
+				if (item) {
+					array.push(item._id);
+				}
+			});
 
-  const isUserSignedIn = () => {
-    onAuthStateChanged(auth, user => {
-      if (user != null) {
-        // User is signed in
-        setIsLoggedIn(true);
-        setUseId(user.uid);
-      } else {
-        // User is signed out
-        setIsLoggedIn(false);
-      }
-    });
-  };
+			return array;
+		});
 
-  useEffect(() => {
-    isUserSignedIn();
-  }, []);
+		setWishlist((prev) => {
+			const array = [];
+			const temp = JSON.parse(localStorage.getItem("wishlist")) || [];
 
-  useEffect(
-    () => {
-      onHrefChange();
-      window.scrollTo(0, 0);
-    },
-    [pathname]
-  );
+			temp.map((t) => {
+				const item = _products.find(({ _id }) => _id === t);
+				if (item) {
+					array.push(item._id);
+				}
+			});
 
-  useEffect(
-    () => {
-      getData();
-    },
-    [reflesh]
-  );
+			return array;
+		});
+	};
 
-  return (
-    <div className="app">
-      <appContext.Provider
-        value={{
-          products,
-          categories,
-          settings,
-          userId,
-          dataLoading: dataLoading,
-          ordersCtx: [orders, setOrders],
-          refleshCtx: [reflesh, setReflesh],
-          wishlistCtx: [wishlist, setWishlist],
+	const isUserSignedIn = () => {
+		onAuthStateChanged(auth, (user) => {
+			if (user != null) {
+				// User is signed in
+				setIsLoggedIn(true);
+				setUseId(user.uid);
+			} else {
+				// User is signed out
+				setIsLoggedIn(false);
+			}
+		});
+	};
 
-          quickViewCtx: {
-            quickView,
-            setQuickView,
-            setQuickViewProduct,
-            quickViewProduct
-          },
+	useEffect(() => {
+		isUserSignedIn();
+	}, []);
 
-          cartCtx: {
-            cartProducts,
-            setCartProducts,
-            openCart,
-            setOpenCart
-          }
-        }}
-      >
-        <SkeletonTheme
-          baseColor="#f1f1f1"
-          highlightColor="white"
-          direction="right"
-        >
-          {ischeckout ? "" : isAdmin ? <Sidebar /> : <Navbar />}
+	useEffect(() => {
+		onHrefChange();
+		window.scrollTo(0, 0);
+	}, [pathname]);
 
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/:id" element={<ItemView />} />
-            <Route path="/products" element={<ProductsView />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/check-out-status" element={<CheckedOut />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/wishlist" element={<Wishlist />} />
+	useEffect(() => {
+		getData();
+	}, [reflesh]);
 
-            {!isLoggedIn && <Route path="/admin" element={<Login />} />}
+	return (
+		<div className="app">
+			<appContext.Provider
+				value={{
+					products,
+					categories,
+					settings,
+					userId,
+					dataLoading: dataLoading,
+					ordersCtx: [orders, setOrders],
+					refleshCtx: [reflesh, setReflesh],
+					wishlistCtx: [wishlist, setWishlist],
 
-            {/*Dashbord paths  */}
-            {isLoggedIn &&
-              <Route path="/admin">
-                <Route index element={<Admin />} />
-                <Route path="dashboard" element={<Dashboard />} />
-                <Route path="products" element={<Products />} />
-                <Route path="products/edit" element={<EditProduct />} />
-                <Route path="products/create" element={<CreateProduct />} />
-                <Route path="categories" element={<Categories />} />
-                <Route path="settings" element={<Settings />} />
-                <Route path="orders" element={<Orders />} />
-              </Route>}
+					quickViewCtx: {
+						quickView,
+						setQuickView,
+						setQuickViewProduct,
+						quickViewProduct,
+					},
 
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+					cartCtx: {
+						cartProducts,
+						setCartProducts,
+						openCart,
+						setOpenCart,
+					},
+				}}>
+				<SkeletonTheme
+					baseColor="#f1f1f1"
+					highlightColor="white"
+					direction="right">
+					{ischeckout ? "" : isAdmin ? <Sidebar /> : <Navbar />}
 
-          {!isAdmin && !ischeckout && <Footer />}
-          {quickView && <QuickView />}
-          {isError &&
-            <ErrorToast
-              trigger={isError}
-              setTrigger={setIsError}
-              error={error}
-            />}
-          <CartPopup />
-        </SkeletonTheme>
-      </appContext.Provider>
-    </div>
-  );
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/:id" element={<ItemView />} />
+						<Route path="/products" element={<ProductsView />} />
+						<Route path="/checkout" element={<Checkout />} />
+						<Route
+							path="/check-out-status"
+							element={<CheckedOut />}
+						/>
+						<Route path="/cart" element={<Cart />} />
+						<Route path="/wishlist" element={<Wishlist />} />
+
+						{!isLoggedIn && (
+							<Route path="/admin" element={<Login />} />
+						)}
+
+						{/*Dashbord paths  */}
+						{isLoggedIn && (
+							<Route path="/admin">
+								<Route index element={<Admin />} />
+								<Route
+									path="dashboard"
+									element={<Dashboard />}
+								/>
+								<Route path="products" element={<Products />} />
+								<Route
+									path="products/edit"
+									element={<EditProduct />}
+								/>
+								<Route
+									path="products/create"
+									element={<CreateProduct />}
+								/>
+								<Route
+									path="categories"
+									element={<Categories />}
+								/>
+								<Route path="settings" element={<Settings />} />
+								<Route path="orders" element={<Orders />} />
+							</Route>
+						)}
+
+						<Route path="*" element={<NotFound />} />
+					</Routes>
+
+					{!isAdmin && !ischeckout && <Footer />}
+					{quickView && <QuickView />}
+					{isError && (
+						<ErrorToast
+							trigger={isError}
+							setTrigger={setIsError}
+							error={error}
+						/>
+					)}
+					<CartPopup />
+				</SkeletonTheme>
+			</appContext.Provider>
+		</div>
+	);
 }
 
 export default App;
