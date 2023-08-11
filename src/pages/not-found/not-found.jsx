a15952718c1f4e4b66a4efe3ba93002d821a73bd
Mushishi Dickson <redgekson@gmail.com>
2023-08-11 07:42:10 +0200

    v9

--- src/pages/not-found/not-found.jsx
+++ src/pages/not-found/not-found.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import "./not-found.css";
 
 function NotFound() {
-  return (
-    <div className="not-found">
-      <div className="max-width">
-        <div className="nf-cont">
-          <img src="../../../public/404.png" alt="" />
-          <h1>Page Not Found!</h1>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="not-found">
+			<div className="max-width">
+				<div className="nf-cont">
+					<img src="404.png" alt="" />
+					<h1>Page Not Found!</h1>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default NotFound;
