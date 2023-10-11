72b8eee866757959db15dfec900b42e1f9a32c3e
Kson 99 <redgekson@gmail.com>
2023-10-11 19:18:13 +0200

    11/10(v2)

--- src/components/cart/cart-popup.jsx
+++ src/components/cart/cart-popup.jsx
@@ -1,121 +1,121 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./cart-popup.css";
 import IonIcon from "@reacticons/ionicons";
 import CartCard from "../cards/cart-card/cart-card";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { toCurrency } from "../../utils";
 import { appContext } from "../../grobal/context";
 
 function CartPopup() {
-	const { cartCtx, products } = useContext(appContext);
-	const { cartProducts, openCart, setOpenCart } = cartCtx;
+  const { cartCtx, products } = useContext(appContext);
+  const { cartProducts, openCart, setOpenCart } = cartCtx;
 
-	const navigate = useNavigate();
-	const [subtotal, setSubtotal] = useState({});
-	const [checkoutProducts, setCheckoutProducts] = useState({});
-	const [closing, setClosing] = useState(false);
+  const navigate = useNavigate();
+  const [subtotal, setSubtotal] = useState({});
+  const [checkoutProducts, setCheckoutProducts] = useState({});
+  const [closing, setClosing] = useState(false);
+  const { pathname } = useLocation();
 
-	const getCartProducts = () => {
-		let _products = [];
+  const getCartProducts = () => {
+    let _products = [];
 
-		cartProducts.map((prod) => {
-			const pItem = products.find(({ _id }) => _id === prod);
-			_products.push(pItem);
-		});
+    cartProducts.map((prod) => {
+      const pItem = products.find(({ _id }) => _id === prod);
+      _products.push(pItem);
+    });
 
-		return _products;
-	};
+    return _products;
+  };
 
-	const closeCart = () => {
-		setClosing(true);
-		setTimeout(() => {
-			setOpenCart(false);
-			setClosing(false);
-		}, 300);
-	};
+  const closeCart = () => {
+    setClosing(true);
+    setTimeout(() => {
+      setOpenCart(false);
+      setClosing(false);
+    }, 300);
+  };
 
-	const getSubtotal = () => {
-		let _subtotal = 0;
+  const getSubtotal = () => {
+    let _subtotal = 0;
 
-		Object.keys(subtotal).forEach((key, index) => {
-			_subtotal += subtotal[key];
-		});
+    Object.keys(subtotal).forEach((key, index) => {
+      _subtotal += subtotal[key];
+    });
 
-		return _subtotal;
-	};
+    return _subtotal;
+  };
 
-	const checkout = () => {
-		closeCart();
-		navigate("/checkout", {
-			state: {
-				total: getSubtotal(),
-				cartData: checkoutProducts,
-			},
-		});
-	};
+  const checkout = () => {
+    closeCart();
+    navigate("/checkout", {
+      state: {
+        total: getSubtotal(),
+        cartData: checkoutProducts,
+      },
+    });
+  };
 
-	const goToCart = () => {
-		closeCart();
-		navigate("/cart");
-	};
+  const goToCart = () => {
+    closeCart();
+    navigate("/cart");
+  };
 
-	return (
-		openCart && (
-			<div className="cart-popup" onClick={closeCart}>
-				<div
-					className={closing ? "cart-cont closing" : "cart-cont"}
-					onClick={(e) => e.stopPropagation()}>
-					<div className="header">
-						<h4>Shopping Cart ({cartProducts.length})</h4>
-						<IonIcon
-							name="close"
-							className="icon"
-							onClick={closeCart}
-						/>
-					</div>
+  useEffect(() => {
+    closeCart();
+  }, [pathname]);
 
-					<div className="body">
-						{getCartProducts().map((item) => (
-							<CartCard
-								key={item._id}
-								item={item}
-								setSubtotal={setSubtotal}
-								setProducts={setCheckoutProducts}
-							/>
-						))}
-					</div>
+  return (
+    openCart && (
+      <div className="cart-popup" onClick={closeCart}>
+        <div
+          className={closing ? "cart-cont closing" : "cart-cont"}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <div className="header">
+            <h4>Shopping Cart ({cartProducts.length})</h4>
+            <IonIcon name="close" className="icon" onClick={closeCart} />
+          </div>
 
-					<div className="foot">
-						{cartProducts.length > 0 ? (
-							<div className="checkout">
-								<div className="sub-tot">
-									<p>Sub-Total</p>
-									<p>N$ {toCurrency(getSubtotal())}</p>
-								</div>
-								<textarea placeholder="Order note"></textarea>
-								<p>Shipping and Taxes calculated at checkout</p>
-								<button
-									className="submitBtn"
-									onClick={checkout}>
-									<IonIcon name="lock-closed" />
-									<p>Checkout</p>
-								</button>
+          <div className="body">
+            {getCartProducts().map((item) => (
+              <CartCard
+                key={item._id}
+                item={item}
+                setSubtotal={setSubtotal}
+                setProducts={setCheckoutProducts}
+              />
+            ))}
+          </div>
 
-								<p onClick={goToCart} id="to-cart">
-									Go to Cart
-								</p>
-							</div>
-						) : (
-							<div className="cart-empty">
-								<IonIcon name="cart" className="icon" />
-								Your Cart is Empty
-							</div>
-						)}
-					</div>
-				</div>
-			</div>
-		)
-	);
+          <div className="foot">
+            {cartProducts.length > 0 ? (
+              <div className="checkout">
+                <div className="sub-tot">
+                  <p>Sub-Total</p>
+                  <p>N$ {toCurrency(getSubtotal())}</p>
+                </div>
+                <textarea placeholder="Order note"></textarea>
+                <p>Shipping and Taxes calculated at checkout</p>
+                <button className="submitBtn" onClick={checkout}>
+                  <IonIcon name="lock-closed" />
+                  <p>Checkout</p>
+                </button>
+
+                <p onClick={goToCart} id="to-cart">
+                  Go to Cart
+                </p>
+              </div>
+            ) : (
+              <div className="cart-empty">
+                <IonIcon name="cart" className="icon" />
+                Your Cart is Empty
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    )
+  );
 }
 
 export default CartPopup;
